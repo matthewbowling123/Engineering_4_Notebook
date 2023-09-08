6c6f1c07565a6cc573f43c9bcd9ea724e295c0fd
matthewbowling123 <mbowlin48@charlottesvilleschools.org>
2023-09-08 11:55:53 -0400

    Here is code

--- raspberry-pi/Pico_Liftoff.py
+++ raspberry-pi/Pico_Liftoff.py
@@ -2,11 +2,18 @@ import time
 import board
 import digitalio
 
-
+LED1 = digitalio.DigitalInOut(board.GP0)
+LED1.direction = digitalio.Direction.OUTPUT
+LED2 = digitalio.DigitalInOut(board.GP1)
+LED2.direction = digitalio.Direction.OUTPUT
 for x in range(10):
     launch = 10-x
     print(launch)
-    time.sleep(1)
-
-if launch == 1:
-    print("Liftoff")
+    LED1.value = True
+    time.sleep(.5)
+    LED1.value = False
+    time.sleep(.5)
+print("Liftoff")
+LED2.value = True
+time.sleep(5)
+LED2.value = False
