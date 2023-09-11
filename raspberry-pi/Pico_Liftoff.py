7d99518d144428c0ba3952250679fb36b3612719
matthewbowling123 <mbowlin48@charlottesvilleschools.org>
2023-09-11 10:40:34 -0400

    Hello

--- raspberry-pi/Pico_Liftoff.py
+++ raspberry-pi/Pico_Liftoff.py
@@ -1,19 +1,28 @@
-import time
 import board
 import digitalio
+import time
 
-LED1 = digitalio.DigitalInOut(board.GP0)
-LED1.direction = digitalio.Direction.OUTPUT
-LED2 = digitalio.DigitalInOut(board.GP1)
-LED2.direction = digitalio.Direction.OUTPUT
-for x in range(10):
-    launch = 10-x
-    print(launch)
-    LED1.value = True
+led = digitalio.DigitalInOut(board.GP1)         # This sets up the pins for the LED and Buttons.
+led.direction = digitalio.Direction.OUTPUT      
+led2= digitalio.DigitalInOut(board.GP2)
+led2.direction = digitalio.Direction.OUTPUT
+button= digitalio.DigitalInOut(board.GP3)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.DOWN
+
+while button.value == False:
+    print("ready for liftoff")
+
+
+for x in range (10):                            #Print 10-1
+    print(10-x)                                
+    led.value = True                            #blinks on and off every second
     time.sleep(.5)
-    LED1.value = False
+    led.value = False
     time.sleep(.5)
-print("Liftoff")
-LED2.value = True
-time.sleep(5)
-LED2.value = False
+
+print ("liftoff")
+while True:                                     #Need a loop to continue running code after for loop
+    led2.value = True                           #Turns on green led after countdown
+    time.sleep(5)
+    led2.value = True
--- raspberry-pi/test.py
+++ raspberry-pi/test.py
@@ -1,1 +1,28 @@
+import board
+import digitalio
+import time
 
+led = digitalio.DigitalInOut(board.GP1)         #Initializing led pins and outputs
+led.direction = digitalio.Direction.OUTPUT      
+led2= digitalio.DigitalInOut(board.GP2)
+led2.direction = digitalio.Direction.OUTPUT
+button= digitalio.DigitalInOut(board.GP3)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.DOWN
+
+while button.value == False
+print("ready for liftoff")
+
+
+for x in range (10):                            #prints numbers 1-10 in ascending order
+    print(10-x)                                 #changes to print numbers 1-10 in descending order
+    led.value = True                            #blinks on and off every second
+    time.sleep(.5)
+    led.value = False
+    time.sleep(.5)
+
+print ("liftoff")
+while True:                                     #Need a loop to continue running code after for loop
+    led2.value = True                           #Turns on green led after countdown
+    time.sleep(5)
+    led2.value = True
